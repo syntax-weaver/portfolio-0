350bba6e249cc59276357d1b74073ce613ab98fd
asmaa-othman-1 <starb3936@gmail.com>
2025-09-20 22:29:13 +0300

    Initial commit - Portfolio-0

---
+++ main.js
@@ -0,0 +1,10 @@
+tsParticles.load("tsparticles", {
+    particles: {
+      number: { value: 100 },
+      size: { value: 2 },
+      move: { enable: true, speed: 0.3 },
+      opacity: { value: 0.3 },
+      color: { value: "#ffffff" },
+    },
+    background: { color: "#0d1117" }
+  });
